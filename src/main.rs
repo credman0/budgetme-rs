ac5d772807ed2ea87462e2c1d30fa1611ce24186
Colin Redman <20376935+credman0@users.noreply.github.com>
2021-02-03 21:03:01 -0700

    Add aws online storage

--- src/main.rs
+++ src/main.rs
@@ -6,6 +6,10 @@ use chrono::prelude::*;
 use colored::*;
 use serde::{Deserialize, Serialize};
 use derivative::Derivative;
+use rusoto_core::{Region,credential::StaticProvider};
+use rusoto_s3::{S3, S3Client, CreateBucketRequest, PutObjectRequest, GetObjectRequest};
+use async_trait::async_trait;
+use tokio::{runtime, io::AsyncReadExt};
 
 // (Buf) Uncomment these lines to have the output buffered, this can provide
 // better performance but is not always intuitive behaviour.
@@ -39,6 +43,12 @@ enum Command {
     }
 }
 
+#[derive(Serialize, Deserialize, PartialEq, Clone)]
+enum DataSource {
+    Local(LocalDataProvider),
+    Aws(AwsS3DataProviderFactory)
+}
+
 #[derive(StructOpt, Debug)]
 enum CfgCommand {
     /// Valid keys are rate and path
@@ -46,7 +56,7 @@ enum CfgCommand {
         key:String,
         value:String
     },
-    /// Valid keys are rate and path
+    /// Valid keys are [rate, path, access_key, secret_access_key, 
     Get {
         key:String
     }
@@ -55,12 +65,12 @@ enum CfgCommand {
 #[derive(Serialize, Deserialize)]
 struct Config {
     rate:f32,
-    data_path:Option<String>
+    data_source:Option<DataSource>
 }
 
 impl Config {
     fn new() -> Config {
-        return Config {rate:2.5, data_path:None};
+        return Config {rate:2.5, data_source:None};
     }
 }
 #[derive(Derivative, Serialize, Deserialize, Clone)]
@@ -263,6 +273,124 @@ impl Budget {
     }
 }
 
+#[async_trait]
+trait DataProvider {
+    async fn get(&self) -> Option<Data>;
+    async fn put(&self,data:&Data);
+}
+
+// serializable configuration that can produce a data provider
+trait DataProviderFactory {
+    fn to_provider(self) -> Box<dyn DataProvider>;
+}
+
+#[derive(Serialize, Deserialize, PartialEq, Clone)]
+struct LocalDataProvider {
+    file_path:PathBuf
+}
+
+#[async_trait]
+impl DataProvider for LocalDataProvider {
+    async fn get(&self) -> Option<Data> {
+        if self.file_path.exists() {
+            let data:Data = serde_json::from_str(&fs::read_to_string(&self.file_path).unwrap()).unwrap();
+            return Some(data);
+        } else {
+            return None;
+        }
+    }
+
+    async fn put(&self, data:&Data) {
+        fs::write(&self.file_path, serde_json::to_string(&data).unwrap()).unwrap();
+    }
+}
+
+impl DataProviderFactory for LocalDataProvider {
+    fn to_provider(self) -> Box<dyn DataProvider> {
+        return Box::new(self);
+    }
+}
+
+impl LocalDataProvider {
+    fn new(file_path:PathBuf) -> LocalDataProvider {
+        return LocalDataProvider{file_path:file_path}
+    }
+}
+
+struct AwsS3DataProvider {
+    s3:S3Client,
+    bucket_name:String
+}
+
+#[async_trait]
+impl DataProvider for AwsS3DataProvider {
+    async fn get(&self) -> Option<Data> {
+        let get_obj_req = GetObjectRequest {
+            bucket: self.bucket_name.clone(),
+            key: "data.json".to_string(),
+            ..Default::default()
+        };
+        let result = self.s3.get_object(get_obj_req).await;
+        if result.is_err() {
+            return None;
+        } else {
+            let stream = result.unwrap().body.unwrap();
+            let mut buffer = String::new();
+            stream.into_async_read().read_to_string(&mut buffer).await.unwrap();
+            let data:Data = serde_json::from_str(&buffer).unwrap();
+            return Some(data);
+        }
+    }
+
+    async fn put(&self, data:&Data) {
+        self.create_bucket().await;
+        let contents:Vec<u8> = serde_json::to_string(&data).unwrap().as_bytes().to_vec();
+        let put_request = PutObjectRequest {
+            bucket: self.bucket_name.to_owned(),
+            key: "data.json".to_string(),
+            body: Some(contents.into()),
+            ..Default::default()
+        };
+        self.s3
+            .put_object(put_request)
+            .await
+            .expect("Failed to put data object");
+    }
+}
+
+impl AwsS3DataProvider {
+    async fn create_bucket(&self) {
+        let create_bucket_req = CreateBucketRequest {
+            bucket: self.bucket_name.clone(),
+            ..Default::default()
+        };
+        self.s3
+            .create_bucket(create_bucket_req)
+            .await
+            .expect("Failed to create test bucket");
+    }
+}
+
+#[derive(Serialize, Deserialize, PartialEq, Clone)]
+struct AwsS3DataProviderFactory {
+    access_key:String,
+    secret_access_key:String,
+    bucket_name:String,
+    region:Region,
+}
+
+impl DataProviderFactory for AwsS3DataProviderFactory {
+    fn to_provider(self) -> Box<dyn DataProvider> {
+        return Box::new(AwsS3DataProvider{bucket_name:self.bucket_name.clone(), 
+            s3:S3Client::new_with(
+                rusoto_core::request::HttpClient::new().expect("Failed to create HTTP client"),
+                StaticProvider::new(self.access_key, self.secret_access_key, None, None),
+                self.region.clone(),
+            )}
+        );
+    }
+}
+
 #[cfg(not(target_os="windows"))]
 fn prepare_virtual_terminal() {
 }
@@ -291,12 +419,12 @@ fn main() {
         data_path = Path::new(&path).join("budgetme");
     }
     let mut full_data_path = data_path.join("data.json");
-    let data:Data;
-    if full_data_path.exists() {
-        data = serde_json::from_str(&fs::read_to_string(&full_data_path).unwrap()).unwrap();
-    } else {
-        data = Data::new();
-    }
+    //let mut data_provider:LocalDataProvider = LocalDataProvider::new(full_data_path.clone());
+    let data_provider:&DataProvider = &*AwsS3DataProviderFactory {access_key:"AKIA5S65SRCS2XZIQ5FF".to_string(), secret_access_key:"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".to_string(), bucket_name:"budgetdfasdfasdfasdfasdfasdf".to_string(), region:Region::UsEast1}.to_provider();
+    let maybe_data = data_provider.get();
+    let data:Data = runtime::Runtime::new().unwrap().block_on(async {
+        maybe_data.await.unwrap_or(Data::new())
+    });
     let mut budget = Budget {config:config, data:data};
     budget.data.update(&budget.config.rate);
     if args.command.is_none() {
@@ -316,18 +444,19 @@ fn main() {
     fs::create_dir_all(base_dir).unwrap();
     if budget.config.data_path.is_some() {
         fs::create_dir_all((&budget).config.data_path.as_ref().unwrap()).unwrap();
+        // update the full path because it might have changed during configuration
         full_data_path = PathBuf::from((&budget.config).data_path.as_ref().unwrap()).join("data.json");
     }
+    //data_provider.file_path = full_data_path;
     fs::write(&config_path, serde_json::to_string(&budget.config).unwrap()).unwrap();
-    let old_data:Data;
-    if full_data_path.exists() {
-        old_data = serde_json::from_str(&fs::read_to_string(&full_data_path).unwrap()).unwrap();
-    } else {
-        old_data = Data::new();
-    }
-    if budget.verify_against(old_data) {
-        fs::write(&full_data_path, serde_json::to_string(&budget.data).unwrap()).unwrap();
-    } else {
-        println!("{}", "Refusing to overwrite unrelated histories".red().on_black());
-    }
+    let maybe_old_data = data_provider.get();
+    runtime::Runtime::new().unwrap().block_on(async {
+        let old_data:Data = maybe_old_data.await.unwrap_or(Data::new());
+        if budget.verify_against(old_data) {
+            data_provider.put(&budget.data).await;
+            //fs::write(&full_data_path, serde_json::to_string(&budget.data).unwrap()).unwrap();
+        } else {
+            println!("{}", "Refusing to overwrite unrelated histories".red().on_black());
+        }
+    });
 }
